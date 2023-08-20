de9e34a1c90ed22492d9fd2f7cf9be9a491709cb
shinkuan <shinkuan318@gmail.com>
2023-08-20 15:58:45 +0800

    This is branch for Mortal

--- bot.py
+++ bot.py
@@ -1,14 +1,44 @@
-#!/usr/bin/env python3
+import json
+import sys
 
-import warnings
-from _kanachan import Kanachan
+from loguru import logger
 
-
-def main() -> None:
-    kanachan = Kanachan()
-    kanachan.run()
+import model
 
 
-if __name__ == '__main__':
-    warnings.filterwarnings("ignore")
+class Bot:
+    def __init__(self, player_id: int):
+        self.player_id = player_id
+        self.model = model.load_model(player_id)
+
+    def react(self, events: str) -> str:
+        events = json.loads(events)
+
+        # logger.info("hi")
+        return_action = None
+        for e in events:
+            return_action = self.model.react(json.dumps(e, separators=(",", ":")))
+
+        if return_action is None:
+            return json.dumps({"type":"none"}, separators=(",", ":"))
+        else:
+            raw_data = json.loads(return_action)
+            del raw_data["meta"]
+            return json.dumps(raw_data, separators=(",", ":"))
+
+
+def main():
+    player_id = int(sys.argv[1])
+    assert player_id in range(4)
+    bot = Bot(player_id)
+
+    while True:
+        line = sys.stdin.readline().strip()
+        resp = bot.react(line)
+        sys.stdout.write(resp + "\n")
+        sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    # debug()
     main()
--- create_player.bat
+++
@@ -1,2 +0,0 @@
-tar.exe -a -c -f kanachan.zip bot.py game.json hand_calculator.py kanachan model _kanachan.py
-move /Y kanachan.zip players\kanachan.zip
