fedc9ad7343902a131d5d195da98f53322e2f23b
shinkuan <shinkuan318@gmail.com>
2023-08-20 16:44:20 +0800

    Done

--- model.py
+++ model.py
@@ -1,167 +1,301 @@
-import json
-from typing import Optional
-from loguru import logger
-
 import numpy as np
 import torch
-from torch import nn
-from torch.nn import functional as F
+from torch import nn, Tensor
+from torch.nn import init, functional as F
+from torch.nn.utils.rnn import pack_padded_sequence, pad_sequence
 from torch.distributions import Normal, Categorical
+from typing import *
+from functools import partial
+from itertools import permutations
 from libriichi.mjai import Bot
 from libriichi.consts import obs_shape, oracle_obs_shape, ACTION_SPACE, GRP_SIZE
 
-
-@torch.jit.script
-def apply_masks(actions, masks, fill: float = -1e9):
-    fill = torch.tensor(fill, dtype=actions.dtype, device=actions.device)
-    return torch.where(masks, actions, fill)
-
-
 class ChannelAttention(nn.Module):
-    def __init__(self, channels, ratio=16):
+    def __init__(self, channels, ratio=16, actv_builder=nn.ReLU):
         super().__init__()
-        self.avg = nn.AdaptiveAvgPool1d(1)
-        self.max = nn.AdaptiveMaxPool1d(1)
         self.shared_mlp = nn.Sequential(
             nn.Linear(channels, channels // ratio),
-            nn.ReLU(inplace=True),
+            actv_builder(),
             nn.Linear(channels // ratio, channels),
         )
+        for mod in self.modules():
+            if isinstance(mod, nn.Linear):
+                init.constant_(mod.bias, 0)
 
     def forward(self, x):
-        avg_out = self.avg(x).squeeze(-1)
-        max_out = self.max(x).squeeze(-1)
-        avg_out = self.shared_mlp(avg_out)
-        max_out = self.shared_mlp(max_out)
-        out = torch.sigmoid(avg_out + max_out).unsqueeze(-1)
-        return out
-
+        avg_out = self.shared_mlp(x.mean(-1))
+        max_out = self.shared_mlp(x.amax(-1))
+        weight = (avg_out + max_out).sigmoid()
+        x = weight.unsqueeze(-1) * x
+        return x
 
 class ResBlock(nn.Module):
-    def __init__(self, channels, enable_bn, bn_momentum):
+    def __init__(
+        self,
+        channels,
+        *,
+        norm_builder = nn.Identity,
+        actv_builder = nn.ReLU,
+        pre_actv = False,
+        bias = True,
+    ):
         super().__init__()
-        tch_bn_momentum = None
-        if bn_momentum is not None:
-            tch_bn_momentum = 1 - bn_momentum
+        self.actv = actv_builder()
+        self.pre_actv = pre_actv
 
-        self.res_unit = nn.Sequential(
-            nn.Conv1d(channels, channels, kernel_size=3, padding=1, bias=not enable_bn),
-            nn.BatchNorm1d(channels, momentum=tch_bn_momentum) if enable_bn else nn.Identity(),
-            nn.ReLU(inplace=True),
-            nn.Conv1d(channels, channels, kernel_size=3, padding=1, bias=not enable_bn),
-            nn.BatchNorm1d(channels, momentum=tch_bn_momentum) if enable_bn else nn.Identity(),
-        )
-        self.ca = ChannelAttention(channels)
+        if pre_actv:
+            self.res_unit = nn.Sequential(
+                norm_builder(),
+                actv_builder(),
+                nn.Conv1d(channels, channels, kernel_size=3, padding=1, bias=bias),
+                norm_builder(),
+                actv_builder(),
+                nn.Conv1d(channels, channels, kernel_size=3, padding=1, bias=bias),
+            )
+        else:
+            self.res_unit = nn.Sequential(
+                nn.Conv1d(channels, channels, kernel_size=3, padding=1, bias=bias),
+                norm_builder(),
+                actv_builder(),
+                nn.Conv1d(channels, channels, kernel_size=3, padding=1, bias=bias),
+                norm_builder(),
+            )
+        self.ca = ChannelAttention(channels, actv_builder=actv_builder)
 
     def forward(self, x):
         out = self.res_unit(x)
-        out = self.ca(out) * out
-        out += x
-        out = F.relu(out, inplace=True)
+        out = self.ca(out)
+        out = out + x
+        if not self.pre_actv:
+            out = self.actv(out)
         return out
 
-
 class ResNet(nn.Module):
-    def __init__(self, in_channels, conv_channels, num_blocks, enable_bn, bn_momentum):
+    def __init__(
+        self,
+        in_channels,
+        conv_channels,
+        num_blocks,
+        *,
+        norm_builder = nn.Identity,
+        actv_builder = nn.ReLU,
+        pre_actv = False,
+        bias = True,
+    ):
         super().__init__()
-        tch_bn_momentum = None
-        if bn_momentum is not None:
-            tch_bn_momentum = 1 - bn_momentum
 
         blocks = []
         for _ in range(num_blocks):
-            blocks.append(ResBlock(conv_channels, enable_bn=enable_bn, bn_momentum=bn_momentum))
+            blocks.append(ResBlock(
+                conv_channels,
+                norm_builder = norm_builder,
+                actv_builder = actv_builder,
+                pre_actv = pre_actv,
+                bias = bias,
+            ))
 
-        self.net = nn.Sequential(
-            nn.Conv1d(in_channels, conv_channels, kernel_size=3, padding=1, bias=not enable_bn),
-            nn.BatchNorm1d(conv_channels, momentum=tch_bn_momentum) if enable_bn else nn.Identity(),
-            nn.ReLU(inplace=True),
-            *blocks,
+        layers = [nn.Conv1d(in_channels, conv_channels, kernel_size=3, padding=1, bias=bias)]
+        if pre_actv:
+            layers += [*blocks, norm_builder(), actv_builder()]
+        else:
+            layers += [norm_builder(), actv_builder(), *blocks]
+        layers += [
             nn.Conv1d(conv_channels, 32, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            actv_builder(),
             nn.Flatten(),
             nn.Linear(32 * 34, 1024),
-        )
+        ]
+        self.net = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.net(x)
 
-
 class Brain(nn.Module):
-    def __init__(self, is_oracle, conv_channels, num_blocks, enable_bn, bn_momentum):
+    def __init__(self, *, conv_channels, num_blocks, is_oracle=False, version=1):
         super().__init__()
         self.is_oracle = is_oracle
-        in_channels = obs_shape[0]
-        if is_oracle:
-            in_channels += oracle_obs_shape[0]
+        self.version = version
 
-        if bn_momentum == 0:
-            bn_momentum = None
+        in_channels = obs_shape(version)[0]
+        if is_oracle:
+            in_channels += oracle_obs_shape(version)[0]
+
+        norm_builder = partial(nn.BatchNorm1d, conv_channels, momentum=0.01)
+        bias = False
+        actv_builder = partial(nn.Mish, inplace=True)
+        pre_actv = True
+
+        match version:
+            case 1:
+                actv_builder = partial(nn.ReLU, inplace=True)
+                pre_actv = False
+                self.latent_net = nn.Sequential(
+                    nn.Linear(1024, 512),
+                    nn.ReLU(inplace=True),
+                )
+                self.mu_head = nn.Linear(512, 512)
+                self.logsig_head = nn.Linear(512, 512)
+            case 2:
+                pass
+            case 3:
+                norm_builder = partial(nn.BatchNorm1d, conv_channels, momentum=0.01, eps=1e-3)
+            case _:
+                raise ValueError(f'Unexpected version {self.version}')
+
         self.encoder = ResNet(
-            in_channels,
+            in_channels = in_channels,
             conv_channels = conv_channels,
             num_blocks = num_blocks,
-            enable_bn = enable_bn,
-            bn_momentum = bn_momentum,
+            norm_builder = norm_builder,
+            actv_builder = actv_builder,
+            pre_actv = pre_actv,
+            bias = bias,
         )
-
-        self.latent_net = nn.Sequential(
-            nn.Linear(1024, 512),
-            nn.ReLU(inplace=True),
-        )
-        self.mu_head = nn.Linear(512, 512)
-        self.logsig_head = nn.Linear(512, 512)
 
         # when True, never updates running stats, weights and bias and always use EMA or CMA
         self._freeze_bn = False
 
-    def forward(self, obs, invisible_obs: Optional[torch.Tensor] = None):
+    def forward(self, obs, invisible_obs: Optional[Tensor] = None) -> Union[Tuple[Tensor, Tensor], Tensor]:
         if self.is_oracle:
             assert invisible_obs is not None
             obs = torch.cat((obs, invisible_obs), dim=1)
+        phi = self.encoder(obs)
 
-        encoded = self.encoder(obs)
-        latent_out = self.latent_net(encoded)
-        mu = self.mu_head(latent_out)
-        logsig = self.logsig_head(latent_out)
-        return mu, logsig
+        match self.version:
+            case 1:
+                latent_out = self.latent_net(phi)
+                mu = self.mu_head(latent_out)
+                logsig = self.logsig_head(latent_out)
+                return mu, logsig
+            case 2 | 3:
+                return F.mish(phi)
+            case _:
+                raise ValueError(f'Unexpected version {self.version}')
 
     def train(self, mode=True):
         super().train(mode)
         if self._freeze_bn:
-            for module in self.modules():
-                if isinstance(module, nn.BatchNorm1d):
-                    module.eval()
+            for mod in self.modules():
+                if isinstance(mod, nn.BatchNorm1d):
+                    mod.eval()
                     # I don't think this benefits
                     # module.requires_grad_(False)
         return self
 
     def reset_running_stats(self):
-        for module in self.modules():
-            if isinstance(module, nn.BatchNorm1d):
-                module.reset_running_stats()
+        for mod in self.modules():
+            if isinstance(mod, nn.BatchNorm1d):
+                mod.reset_running_stats()
 
-    def freeze_bn(self, flag):
-        self._freeze_bn = flag
+    def set_bn_attrs(self, **kwargs):
+        for mod in self.modules():
+            if isinstance(mod, nn.BatchNorm1d):
+                for k, v in kwargs.items():
+                    if hasattr(mod, k):
+                        setattr(mod, k, v)
+
+    def freeze_bn(self, value: bool):
+        self._freeze_bn = value
         return self.train(self.training)
 
-
-class DQN(nn.Module):
+class NextRankPredictor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.v_head = nn.Linear(512, 1)
-        self.a_head = nn.Linear(512, ACTION_SPACE)
+        self.net = nn.Sequential(
+            nn.Linear(1024, 256),
+            nn.Mish(inplace=True),
+            nn.Linear(256, 4),
+        )
 
-    def forward(self, latent, mask):
-        v = self.v_head(latent)
-        a = self.a_head(latent)
+    def forward(self, x):
+        logits = self.net(x)
+        return logits
 
-        a_sum = apply_masks(a, mask, fill=0).sum(-1, keepdim=True)
+class DQN(nn.Module):
+    def __init__(self, *, version=1):
+        super().__init__()
+        self.version = version
+        match version:
+            case 1:
+                self.v_head = nn.Linear(512, 1)
+                self.a_head = nn.Linear(512, ACTION_SPACE)
+            case 2 | 3:
+                hidden_size = 512 if version == 2 else 256
+                self.v_head = nn.Sequential(
+                    nn.Linear(1024, hidden_size),
+                    nn.Mish(inplace=True),
+                    nn.Linear(hidden_size, 1),
+                )
+                self.a_head = nn.Sequential(
+                    nn.Linear(1024, hidden_size),
+                    nn.Mish(inplace=True),
+                    nn.Linear(hidden_size, ACTION_SPACE),
+                )
+
+    def forward(self, phi, mask):
+        v = self.v_head(phi)
+        a = self.a_head(phi)
+        a_sum = a.masked_fill(~mask, 0.).sum(-1, keepdim=True)
         mask_sum = mask.sum(-1, keepdim=True)
         a_mean = a_sum / mask_sum
-        q = apply_masks(v + a - a_mean, mask)
+        q = (v + a - a_mean).masked_fill(~mask, -torch.inf)
         return q
 
+class GRP(nn.Module):
+    def __init__(self, hidden_size=64, num_layers=2):
+        super().__init__()
+        self.rnn = nn.GRU(input_size=GRP_SIZE, hidden_size=hidden_size, num_layers=num_layers, batch_first=True)
+        self.fc = nn.Sequential(
+            nn.Linear(hidden_size * num_layers, hidden_size * num_layers),
+            nn.ReLU(inplace=True),
+            nn.Linear(hidden_size * num_layers, 24),
+        )
+        for mod in self.modules():
+            mod.to(torch.float64)
+
+        # perms are the permutations of all possible rank-by-player result
+        perms = torch.tensor(list(permutations(range(4))))
+        perms_t = perms.transpose(0, 1)
+        self.register_buffer('perms', perms)     # (24, 4)
+        self.register_buffer('perms_t', perms_t) # (4, 24)
+
+    # input: [grand_kyoku, honba, kyotaku, s[0], s[1], s[2], s[3]]
+    # grand_kyoku: E1 = 0, S4 = 7, W4 = 11
+    # s is 2.5 at E1
+    # s[0] is score of player id 0
+    def forward(self, inputs):
+        lengths = torch.tensor([t.shape[0] for t in inputs], dtype=torch.int64)
+        inputs = pad_sequence(inputs, batch_first=True)
+        packed_inputs = pack_padded_sequence(inputs, lengths, batch_first=True, enforce_sorted=False)
+        return self.forward_packed(packed_inputs)
+
+    def forward_packed(self, packed_inputs):
+        _, state = self.rnn(packed_inputs)
+        state = state.transpose(0, 1).flatten(1)
+        logits = self.fc(state)
+        return logits
+
+    # (N, 24) -> (N, player, rank_prob)
+    def calc_matrix(self, logits):
+        batch_size = logits.shape[0]
+        probs = logits.softmax(-1)
+        matrix = torch.zeros(batch_size, 4, 4, dtype=probs.dtype)
+        for player in range(4):
+            for rank in range(4):
+                cond = self.perms_t[player] == rank
+                matrix[:, player, rank] = probs[:, cond].sum(-1)
+        return matrix
+
+    # (N, 4) -> (N)
+    def get_label(self, rank_by_player):
+        batch_size = rank_by_player.shape[0]
+        perms = self.perms.expand(batch_size, -1, -1).transpose(0, 1)
+        mappings = (perms == rank_by_player).all(-1).nonzero()
+
+        labels = torch.zeros(batch_size, dtype=torch.int64, device=mappings.device)
+        labels[mappings[:, 1]] = mappings[:, 0]
+        return labels
+    
 
 class MortalEngine:
     def __init__(
@@ -169,6 +303,7 @@ class MortalEngine:
         brain,
         dqn,
         is_oracle,
+        version,
         device = None,
         stochastic_latent = False,
         enable_amp = False,
@@ -177,15 +312,14 @@ class MortalEngine:
         name = 'NoName',
         boltzmann_epsilon = 0,
         boltzmann_temp = 1,
-        use_obs_extend = True,
     ):
+        self.engine_type = 'mortal'
         self.device = device or torch.device('cpu')
-        if isinstance(device, str):
-            self.device = torch.device(device)
-
+        assert isinstance(self.device, torch.device)
         self.brain = brain.to(self.device).eval()
         self.dqn = dqn.to(self.device).eval()
         self.is_oracle = is_oracle
+        self.version = version
         self.stochastic_latent = stochastic_latent
 
         self.enable_amp = enable_amp
@@ -206,33 +340,32 @@ class MortalEngine:
     def _react_batch(self, obs, masks, invisible_obs):
         obs = torch.as_tensor(np.stack(obs, axis=0), device=self.device)
         masks = torch.as_tensor(np.stack(masks, axis=0), device=self.device)
+        invisible_obs = None
         if self.is_oracle:
             invisible_obs = torch.as_tensor(np.stack(invisible_obs, axis=0), device=self.device)
-        else:
-            invisible_obs = None
         batch_size = obs.shape[0]
 
-        mu, logsig = self.brain(obs, invisible_obs)
-        if self.stochastic_latent:
-            latent = Normal(mu, logsig.exp()).sample()
-        else:
-            latent = mu
-        q_out = self.dqn(latent, masks)
+        match self.version:
+            case 1:
+                mu, logsig = self.brain(obs, invisible_obs)
+                if self.stochastic_latent:
+                    latent = Normal(mu, logsig.exp() + 1e-6).sample()
+                else:
+                    latent = mu
+                q_out = self.dqn(latent, masks)
+            case 2 | 3:
+                phi = self.brain(obs)
+                q_out = self.dqn(phi, masks)
 
         if self.boltzmann_epsilon > 0:
-            is_greedy = torch.rand(batch_size, device=self.device) >= self.boltzmann_epsilon
-            logits = apply_masks(q_out / self.boltzmann_temp, masks, fill=-1e9)
-            actions = torch.where(
-                is_greedy,
-                q_out.argmax(-1),
-                Categorical(logits=logits).sample(),
-            )
+            is_greedy = torch.full((batch_size,), 1-self.boltzmann_epsilon, device=self.device).bernoulli().to(torch.bool)
+            logits = (q_out / self.boltzmann_temp).masked_fill(~masks, -torch.inf)
+            actions = torch.where(is_greedy, q_out.argmax(-1), Categorical(logits=logits).sample())
         else:
             is_greedy = torch.ones(batch_size, dtype=torch.bool, device=self.device)
             actions = q_out.argmax(-1)
 
         return actions.tolist(), q_out.tolist(), masks.tolist(), is_greedy.tolist()
-
 
 def load_model(seat: int) -> Bot:
     device = torch.device('cpu')
@@ -241,15 +374,9 @@ def load_model(seat: int) -> Bot:
 
     # latest binary model
     control_state_file = "./mortal.pth"
-    resnet_configs = dict(
-        conv_channels = 192,
-        num_blocks = 40,
-        enable_bn = True,
-        bn_momentum = 0.99
-    )
 
-    mortal = Brain(False, **resnet_configs).eval()
-    dqn = DQN().eval()
+    mortal = Brain(version=3, conv_channels=192, num_blocks=40).eval()
+    dqn = DQN(version=3).eval()
     state = torch.load(control_state_file, map_location=torch.device('cpu'))
     mortal.load_state_dict(state['mortal'])
     dqn.load_state_dict(state['current_dqn'])
@@ -263,7 +390,7 @@ def load_model(seat: int) -> Bot:
         enable_quick_eval = False,
         enable_rule_based_agari_guard = True,
         name = 'mortal',
-        use_obs_extend = True,
+        version= 3
     )
 
     bot = Bot(engine, seat)
